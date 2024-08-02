bd7847d466c3b15606c3420712c2635f8d9119a1
Bethelhem Daniel <82999200+Bethel-21@users.noreply.github.com>
2024-08-02 16:22:16 +0300

    Added the search functionality using the API

--- src/script.js
+++ src/script.js
@@ -1,8 +1,26 @@
+function displayTemperature(response) {
+  let changeCityContent = document.querySelector(".city-place");
+  let cityElement = response.data.city;
+  changeCityContent.innerHTML = cityElement;
+  let temperature = Math.round(response.data.temperature.current);
+  let changeTemperature = document.querySelector(".temperature-value");
+  changeTemperature.innerHTML = `${temperature}ºC`;
+}
+
+function listenRequest(city) {
+  let apiKey = "70dfc7dao135cdc12280t4b9f7470760";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
 function changeCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".search-box");
-  let changeCity = document.querySelector(".city-place");
-  changeCity.innerHTML = searchInput.value;
+
+  listenRequest(searchInput.value);
 }
+
 let change = document.querySelector(".form-place");
 change.addEventListener("submit", changeCity);
+
+listenRequest("Addis Ababa");

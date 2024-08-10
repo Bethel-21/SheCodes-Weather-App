349d75b5e73123a1a069703711bc266fe9fe7783
Bethelhem Daniel <82999200+Bethel-21@users.noreply.github.com>
2024-08-10 13:02:22 +0300

    Integrated the API for the forecast

--- src/script.js
+++ src/script.js
@@ -1,47 +1,3 @@
-function displayTemperature(response) {
-  let changeCityContent = document.querySelector(".city-place");
-  let cityElement = response.data.city;
-  changeCityContent.innerHTML = cityElement;
-  let temperature = Math.round(response.data.temperature.current);
-  let wind = Math.round(response.data.wind.speed);
-  let humidity = Math.round(response.data.temperature.humidity);
-  let condition = response.data.condition.description;
-  let iconValue = response.data.condition.icon_url;
-
-  let changeTemperature = document.querySelector(".temperature-value");
-  changeTemperature.innerHTML = `${temperature}ºC`;
-
-  let changeWind = document.querySelector(".wind-value");
-  changeWind.innerHTML = `${wind}km/hr`;
-
-  let changeHumidity = document.querySelector(".humidity-value");
-  changeHumidity.innerHTML = `${humidity}%`;
-
-  let changeCondition = document.querySelector(".weather-condition");
-  changeCondition.innerHTML = `${condition}`;
-
-  let changeIcon = document.querySelector(".icon");
-  changeIcon.src = `${iconValue}`;
-}
-
-function listenRequest(city) {
-  let apiKey = "70dfc7dao135cdc12280t4b9f7470760";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(displayTemperature);
-}
-
-function changeCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector(".search-box");
-
-  listenRequest(searchInput.value);
-}
-
-let change = document.querySelector(".form-place");
-change.addEventListener("submit", changeCity);
-
-listenRequest("Addis Ababa");
-
 let dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 let monthNames = [
   "Jan",
@@ -84,23 +40,86 @@ updateDateTime();
 
 setInterval(updateDateTime, 1000);
 
-function showForecast() {
+function displayTemperature(response) {
+  let changeCityContent = document.querySelector(".city-place");
+  let cityElement = response.data.city;
+  changeCityContent.innerHTML = cityElement;
+  let temperature = Math.round(response.data.temperature.current);
+  let wind = Math.round(response.data.wind.speed);
+  let humidity = Math.round(response.data.temperature.humidity);
+  let condition = response.data.condition.description;
+  let iconValue = response.data.condition.icon_url;
+
+  let changeTemperature = document.querySelector(".temperature-value");
+  changeTemperature.innerHTML = `${temperature}ºC`;
+
+  let changeWind = document.querySelector(".wind-value");
+  changeWind.innerHTML = `${wind}km/hr`;
+
+  let changeHumidity = document.querySelector(".humidity-value");
+  changeHumidity.innerHTML = `${humidity}%`;
+
+  let changeCondition = document.querySelector(".weather-condition");
+  changeCondition.innerHTML = `${condition}`;
+
+  let changeIcon = document.querySelector(".icon");
+  changeIcon.src = `${iconValue}`;
+  displayForecast(response.data.city);
+}
+
+function listenRequest(city) {
+  let apiKey = "70dfc7dao135cdc12280t4b9f7470760";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function changeCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector(".search-box");
+
+  listenRequest(searchInput.value);
+}
+
+let change = document.querySelector(".form-place");
+change.addEventListener("submit", changeCity);
+
+listenRequest("Addis Ababa");
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
+function showForecast(response) {
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="days-container">
-          <div class="forecast-day">${day}</div>
-          <div class="forecast-icon">⛅</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="days-container">
+          <div class="forecast-day">${formatDate(day.time)}</div>
+          <img src = "${day.condition.icon_url}" class = "forecast-icon"></img>
           <div class="forecast-value">
-            <strong class="weather-upper-range">19ºC</strong>
-            <span class="weather-lower-range">16ºC</span>
+            <strong class="weather-upper-range">${Math.round(
+              day.temperature.maximum
+            )}ºC</strong>
+            <span class="weather-lower-range">${Math.round(
+              day.temperature.minimum
+            )}ºC</span>
           </div>
         </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#weather-forecast-id");
   forecastElement.innerHTML = forecastHtml;
 }
 showForecast();
+
+function displayForecast(city) {
+  let apiKey = "70dfc7dao135cdc12280t4b9f7470760";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(showForecast);
+}
